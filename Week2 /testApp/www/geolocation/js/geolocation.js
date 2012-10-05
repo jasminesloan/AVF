29550b645f799d8a0f6a5467d6fabe0b3fb95ff6
jasminesloan <jasminesloan@fullsail.edu>
2012-10-05 19:02:48 -0400

    Submitting files for week 2 assignment

--- Week2 /testApp/www/geolocation/js/geolocation.js
+++ Week2 /testApp/www/geolocation/js/geolocation.js
@@ -12,8 +12,8 @@ function showPosition(position)
 {
     lat=position.coords.latitude;
     lon=position.coords.longitude;
-    latlon=new google.maps.LatLng(lat, lon)
-    mapholder=document.getElementById('mapholder')
+    latlon=new google.maps.LatLng(lat, lon);
+    mapholder=document.getElementById('mapholder');
     mapholder.style.height='250px';
     mapholder.style.width='500px';
     
@@ -32,16 +32,16 @@ function showError(error)
     switch(error.code)
     {
         case error.PERMISSION_DENIED:
-            x.innerHTML="User denied the request for Geolocation."
+            x.innerHTML="User denied the request for Geolocation.";
             break;
         case error.POSITION_UNAVAILABLE:
-            x.innerHTML="Location information is unavailable."
+            x.innerHTML="Location information is unavailable.";
             break;
         case error.TIMEOUT:
-            x.innerHTML="The request to get user location timed out."
+            x.innerHTML="The request to get user location timed out.";
             break;
         case error.UNKNOWN_ERROR:
-            x.innerHTML="An unknown error occurred."
+            x.innerHTML="An unknown error occurred.";
             break;
     }
 }
